c5de056bd8ab81898e75588ddc445f6c479bc044
Oscar Lagatta <oscarlagatta@gmail.com>
2025-11-03 09:05:22 +0000

    initial commit

---
+++ app/account/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'Account'
+}
+
+export default function Page() {
+    return (<div>Account</div>);
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,9 @@
+
+export const metadata = {
+    title: 'Cabins'
+}
+export default function Page() {
+    return (<div>
+        <h1>Cabins</h1>
+    </div>);
+}
